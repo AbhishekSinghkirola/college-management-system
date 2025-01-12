865d77c3988b3857ce71e5e313ec3434a8a2c66c
rawat9015 <nr7584128@gmail.com>
2025-01-12 20:42:41 +0530

    Edit & Delete Student Functionality

--- application/models/Students_model.php
+++ application/models/Students_model.php
@@ -27,61 +27,22 @@ class Students_model extends CI_Model
         }
     }
 
-    public function delete_category($cat_id)
-    {
-        if ($cat_id) {
-            return $this->db->delete('category', ['category_id' => $cat_id]);
-        }
-    }
-
-    public function update_category($update_array)
-    {
-        $cat_id = $update_array['cat_id'];
-
-        if ($cat_id) {
-            unset($update_array['cat_id']);
-            return $this->db->update('category', $update_array, ['category_id' => $cat_id]);
-        }
-    }
-
-    public function get_courses($course_id = null)
+    public function update_student($update_array)
     {
 
-        $this->db->from('courses');
-        $this->db->join('category', 'courses.course_category=category.category_id');
-
-        if ($course_id) {
-            $this->db->where('id', $course_id);
-            $res = $this->db->get()->row_array();
-        } else {
-            $res = $this->db->get()->result_array();
-        }
-
-        if ($res) {
-            return $res;
-        }
-    }
-
-    public function add_course($insert_data)
-    {
-        return $this->db->insert('courses', $insert_data);
-    }
-
-    public function delete_course($course_id)
-    {
-        if ($course_id) {
-            return $this->db->delete('courses', ['id' => $course_id]);
-        }
-    }
-
-
-    public function update_course($update_array)
-    {
-        $id = $update_array['id'];
+        $id = $update_array['student_id'];
 
         if ($id) {
-            unset($update_array['id']);
-            return $this->db->update('courses', $update_array, ['id' => $id]);
+            unset($update_array['student_id']);
+            return $this->db->update('student', $update_array, ['student_id' => $id]);
         }
     }
+
+    public function delete_student($student_id)
+    {
+        if ($student_id) {
+            return $this->db->delete('student', ['student_id' => $student_id]);
+        }
+    }
+     
 }
